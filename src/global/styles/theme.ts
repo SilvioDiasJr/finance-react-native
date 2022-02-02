e63e6abe37e7312f2f55c1de99309a84bd78618b
Silvio <silvio.dias@etec.sp.gov.br>
2022-02-02 00:41:27 -0300

    feat: Adding new styles

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,3 +1,25 @@
-export default{
-  colors:{}
+export default {
+  colors: {
+    main: '#0A1633',
+
+    white: '#FFFFFF',
+    black: '#000000',
+
+    alert: '#F94747',
+    success: '#4BD1A0',
+
+    title: '#000000',
+    icon: '#858585',
+
+    card_primary: '#F5F5F5',
+    card_secondary: '#E7E7E7',
+
+    button_selected: '#9B9B9B',
+
+    background: '#FFFFFF'
+  },
+  fonts: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold'
+  }
 }
