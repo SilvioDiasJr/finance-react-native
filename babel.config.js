fbc94fde4a85e44ee802791b522d34cc93ab1f32
Silvio <silvio.dias@etec.sp.gov.br>
2022-02-04 17:26:24 -0300

    chore: Adding new paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      ['inline-dotenv'],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@global': './src/global',
+            '@assets': './src/assets',
+            '@services': './src/services'
+          }
+        }
+      ]
+    ]
+  }
+}
